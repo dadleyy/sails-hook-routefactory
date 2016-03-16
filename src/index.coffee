6d1f927da80ee4f131ad007b755fe0e8cf036795
Danny Hadley <dadley@sizethreestudios.com>
2016-03-16 16:23:24 -0400

    adding factory.resource functionality + tests - hooking into sails router via "hook.routes.before"

--- src/index.coffee
+++ src/index.coffee
@@ -1,10 +1,14 @@
 module.exports = (sails) ->
 
-  hook = {}
+  hook = {routes: {}}
   generators = {}
 
   upper = (str) -> "#{str or ""}".toUpperCase()
   lower = (str) -> "#{str or ""}".toLowerCase()
+  isarr = (x) -> Array.isArray x
+  isarr.or = (x, o) -> if isarr x then x else o
+  isarr.or.empty = (x) -> isarr.or x, []
+  isarr.or.falsey = (x) -> isarr.or x, false
 
   normalizePath = (parts...) -> (parts.join "/").replace /\/{2,}/g, "/"
 
@@ -12,15 +16,18 @@ module.exports = (sails) ->
     "get", "post", "patch", "destroy:delete"
   ]
 
+  RESOURCE_METHODS =
+    FIND: {method: "get", fn: "find", path: ""}
+    CREATE: {method: "post", fn: "create", path: ""}
+    UPDATE: {method: "patch", fn: "update", path: "/:id"}
+    FIND_ONE: {method: "get", fn: "findOne", path: "/:id"}
+    DESTROY: {method: "delete", fn: "destroy", path: "/:id"}
+
   registerShorthands = (mappings) ->
     for mapping in mappings
       [fn, method] = mapping.split ":"
       method ||= upper fn
       generators[lower fn] = shorthandGenerator method
-
-  generators.resource = (scope) ->
-    handler = () ->
-    handler
 
   generators.group = (scope) ->
     handler = (level, gen) ->
@@ -31,16 +38,49 @@ module.exports = (sails) ->
 
     handler
 
+  makeRoute = (method, path, target) ->
+    result =
+      method: upper method
+      path: path
+      target: target
+    result
+
   generators.route = (scope) ->
     handler = (method_path, mapping) ->
       [method, path] = method_path.split " "
       level_path = normalizePath scope.level, path
-      scope.routes["#{method} #{level_path}"] = mapping
+      route = makeRoute method, level_path, mapping
+      scope.routes.push route
 
     handler
 
   generators.resource = (scope) ->
-    handler = (path, controller) ->
+    exceptions = (extras) ->
+      excludes = isarr.or.empty extras?.excludes
+      whitelist = isarr.or.falsey extras?.only
+
+      handler = (method) ->
+        excluded = (excludes.indexOf method) != -1
+        explicitly_included = whitelist and ~(whitelist.indexOf method)
+        return true if whitelist and not explicitly_included
+        return false if explicitly_included
+        excluded
+
+      handler
+
+    handler = (resource_name, controller, extras) ->
+      resource_path = normalizePath scope.level, resource_name
+      excludes = exceptions extras
+
+      for name, config of RESOURCE_METHODS
+        continue if excludes config.fn
+        action_path = "#{resource_path}#{config.path}"
+        action_mapping = "#{controller}.#{config.fn}"
+        route = makeRoute config.method, action_path, action_mapping
+        scope.routes.push route
+
+      true
+
     handler
 
   shorthandGenerator = (method) ->
@@ -54,7 +94,7 @@ module.exports = (sails) ->
   registerShorthands DEFAULT_SHORTHANDS
 
   factory = (generator) ->
-    scope = {level: "", routes: {}}
+    scope = {level: "", routes: []}
 
     for name, fn of generators
       @[name] = fn(scope)
@@ -63,11 +103,20 @@ module.exports = (sails) ->
 
     scope.routes
 
+  buildRouteConfig = (constructed_routes) ->
+    result = {}
+
+    for route in constructed_routes
+      method_path = "#{upper route.method} #{route.path}"
+      result[method_path] = route.target
+
+    result
+
   hook.initialize = (cb) ->
     applyFactory = ->
       gen = sails.config.routefactory
       routes = factory gen
-
+      hook.routes.before = buildRouteConfig routes
       cb false, routes
 
     sails.after ["hook:userconfig:loaded"], applyFactory
--- test/hook.spec.js
+++ test/hook.spec.js
@@ -4,12 +4,16 @@ var hook = require("../lib"),
 
 describe("sails-hook-routefactory test suite", function() {
 
-  var sails;
+  var sails,
+      bound_routes;
 
   beforeEach(function() {
     sails = { config: {} };
+    bound_routes = {};
 
     sails.after = function(deps, cb) { cb(); }
+    sails.router = {};
+    sails.router.bind = function(path, target) { bound_routes[path] = target; };
   });
 
   describe("when sails.config.routefactory is a function", function() {
@@ -25,6 +29,24 @@ describe("sails-hook-routefactory test suite", function() {
       routes = registered_routes;
     }
 
+    function lookup(method_path) {
+      var route_count = routes ? (routes.length ? routes.length : 0) : 0,
+          parts = method_path.split(" "),
+          route = null,
+          method = parts[0],
+          path = parts[1];
+
+
+      for(var i = 0; i < route_count; i++) {
+        var r = routes[i],
+            m = r.method === method && r.path == path;
+
+        if(m) route = r;
+      }
+
+      return route ? route.target : void 0;
+    }
+
     beforeEach(function() {
       sails.config.routefactory = factory;
       sequence = null;
@@ -37,8 +59,7 @@ describe("sails-hook-routefactory test suite", function() {
       };
 
       hook(sails).initialize(finished);
-
-      expect(routes["GET /stuff"]).to.equal("StuffController.index");
+      expect(lookup("GET /stuff")).to.be("StuffController.index");
     });
 
     it("should bind \"POST /stuff\" on factory.post", function() {
@@ -48,7 +69,7 @@ describe("sails-hook-routefactory test suite", function() {
 
       hook(sails).initialize(finished);
 
-      expect(routes["POST /stuff"]).to.equal("StuffController.create");
+      expect(lookup("POST /stuff")).to.equal("StuffController.create");
     });
 
     it("should bind \"DELETE /stuff\" on factory.destroy", function() {
@@ -58,7 +79,7 @@ describe("sails-hook-routefactory test suite", function() {
 
       hook(sails).initialize(finished);
 
-      expect(routes["DELETE /stuff"]).to.equal("StuffController.destroy");
+      expect(lookup("DELETE /stuff")).to.equal("StuffController.destroy");
     });
 
     it("should bind \"PATCH /stuff\" on factory.patch", function() {
@@ -68,7 +89,399 @@ describe("sails-hook-routefactory test suite", function() {
 
       hook(sails).initialize(finished);
 
-      expect(routes["PATCH /stuff"]).to.equal("StuffController.update");
+      expect(lookup("PATCH /stuff")).to.equal("StuffController.update");
+    });
+
+    describe("when using the resource function", function() {
+
+      describe("when using no extras", function() {
+
+        beforeEach(function() {
+          sequence = function(gen) {
+            gen.resource("users", "UserController");
+          }
+        });
+
+        it("should bind \"POST /users\" to \"UserController.create\"", function() {
+          hook(sails).initialize(finished);
+          expect(lookup("POST /users")).to.be("UserController.create");
+        });
+
+        it("should bind \"GET /users\" to \"UserController.find\"", function() {
+          hook(sails).initialize(finished);
+          expect(lookup("GET /users")).to.equal("UserController.find");
+        });
+
+        it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+          hook(sails).initialize(finished);
+          expect(lookup("GET /users/:id")).to.equal("UserController.findOne");
+        });
+
+        it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+          hook(sails).initialize(finished);
+          expect(lookup("PATCH /users/:id")).to.equal("UserController.update");
+        });
+
+        it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+          hook(sails).initialize(finished);
+          expect(lookup("DELETE /users/:id")).to.equal("UserController.destroy");
+        });
+
+        describe("when using extras", function() {
+
+          var extras;
+
+          beforeEach(function() {
+            extras = {};
+            sequence = function(gen) {
+              gen.resource("users", "UserController", extras);
+            }
+          });
+
+          describe("using an \"excludes\" array", function() {
+
+            describe("[\"create\"]", function() {
+
+              beforeEach(function() {
+                extras.excludes = ["create"];
+              });
+
+              it("should NOT bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be(undefined);
+              });
+
+              it("should bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be("UserController.find");
+              });
+
+              it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.equal("UserController.findOne");
+              });
+
+              it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.equal("UserController.update");
+              });
+
+              it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.equal("UserController.destroy");
+              });
+
+            });
+
+            describe("[\"find\"]", function() {
+
+              beforeEach(function() {
+                extras.excludes = ["find"];
+              });
+
+              it("should bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.equal("UserController.create");
+              });
+
+              it("should NOT bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be(undefined);
+              });
+
+              it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.equal("UserController.findOne");
+              });
+
+              it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.equal("UserController.update");
+              });
+
+              it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.equal("UserController.destroy");
+              });
+
+            });
+
+            describe("[\"findOne\"]", function() {
+
+              beforeEach(function() {
+                extras.excludes = ["findOne"];
+              });
+
+              it("should bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be("UserController.create");
+              });
+
+              it("should bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be("UserController.find");
+              });
+
+              it("should NOT bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be(undefined);
+              });
+
+              it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.equal("UserController.update");
+              });
+
+              it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.equal("UserController.destroy");
+              });
+
+            });
+
+            describe("[\"update\"]", function() {
+
+              beforeEach(function() {
+                extras.excludes = ["update"];
+              });
+
+              it("should bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be("UserController.create");
+              });
+
+              it("should bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be("UserController.find");
+              });
+
+              it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be("UserController.findOne");
+              });
+
+              it("should NOT bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be(undefined);
+              });
+
+              it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be("UserController.destroy");
+              });
+
+            });
+
+            describe("[\"destroy\"]", function() {
+
+              beforeEach(function() {
+                extras.excludes = ["destroy"];
+              });
+
+              it("should bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be("UserController.create");
+              });
+
+              it("should bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be("UserController.find");
+              });
+
+              it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be("UserController.findOne");
+              });
+
+              it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.equal("UserController.update");
+              });
+
+              it("should NOT bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be(undefined);
+              });
+
+            });
+
+          });
+
+          describe("using an \"only\" array", function() {
+
+            describe("[\"create\"]", function() {
+
+              beforeEach(function() {
+                extras = {};
+                extras.only = ["create"];
+              });
+
+              it("should bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be("UserController.create");
+              });
+
+              it("should NOT bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be(undefined);
+              });
+
+              it("should NOT bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be(undefined);
+              });
+
+            });
+
+            describe("[\"find\"]", function() {
+
+              beforeEach(function() {
+                extras.only = ["find"];
+              });
+
+              it("should NOT bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be(undefined);
+              });
+
+              it("should bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be("UserController.find");
+              });
+
+              it("should NOT bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be(undefined);
+              });
+
+            });
+
+            describe("[\"findOne\"]", function() {
+
+              beforeEach(function() {
+                extras.only = ["findOne"];
+              });
+
+              it("should NOT bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be(undefined);
+              });
+
+              it("should NOT bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be(undefined);
+              });
+
+              it("should bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be("UserController.findOne");
+              });
+
+              it("should NOT bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be(undefined);
+              });
+
+            });
+
+            describe("[\"update\"]", function() {
+
+              beforeEach(function() {
+                extras.only = ["update"];
+              });
+
+              it("should NOT bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be(undefined);
+              });
+
+              it("should NOT bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be(undefined);
+              });
+
+              it("should NOT bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be(undefined);
+              });
+
+              it("should bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be("UserController.update");
+              });
+
+              it("should NOT bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be(undefined);
+              });
+
+            });
+
+            describe("[\"destroy\"]", function() {
+
+              beforeEach(function() {
+                extras.only = ["destroy"];
+              });
+
+              it("should NOT bind \"POST /users\" to \"UserController.create\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("POST /users")).to.be(undefined);
+
+              });
+
+              it("should NOT bind \"GET /users\" to \"UserController.find\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users")).to.be(undefined);
+              });
+
+              it("should NOT bind \"GET /users/:id\" to \"UserController.findOne\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("GET /users/:id")).to.be(undefined);
+              });
+
+              it("should NOT bind \"PATCH /users/:id\" to \"UserController.update\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("PATCH /users/:id")).to.be(undefined);
+              });
+
+              it("should bind \"DELETE /users/:id\" to \"UserController.destroy\"", function() {
+                hook(sails).initialize(finished);
+                expect(lookup("DELETE /users/:id")).to.be("UserController.destroy");
+              });
+
+            });
+
+          });
+
+        });
+
+      });
+
     });
 
   });
